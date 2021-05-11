feecc10430c7cf11bde7686d1951d6edd3061fe3
Nilay Mitash <nilay_mitash@hotmail.com>
2021-05-11 08:41:51 -0400

    added json server. Next - api code

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Task} from '../../Task'; //imported to create new tasks
-import {TASKS} from '../../mock-tasks'; //imported to use mock tasks
+import {Task } from '../../Task'; //imported to create new tasks
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -8,11 +8,16 @@ import {TASKS} from '../../mock-tasks'; //imported to use mock tasks
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    //this.tasks = this.taskService.getTasks();
+    //usualy when fetching from backend application, you would wanna use observables instead of simply calling the function
+
+    this.taskService.getTasksFromServer().subscribe((tasks) => (this.tasks = tasks));
+    //pretty much like promises from angularjs
   }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {Task} from '../Task'; //imported to create new tasks
+import {TASKS} from '../mock-tasks'; //imported to use mock tasks
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,15 @@ import { Injectable } from '@angular/core';
 export class TaskService {
 
   constructor() { }
+
+  getTasks(): Task[] {
+    return TASKS;
+  }
+
+  //Note: this is just and example function which returns observable
+  //When using HttpClient, it will return Observable by default
+  getTasksFromServer(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
 }
