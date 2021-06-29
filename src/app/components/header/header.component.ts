0f1dfc3a157607046a0f974cea9c5c4887d01931
Nilay Mitash <nilay_mitash@hotmail.com>
2021-06-29 05:00:46 -0400

    Added routing, footer and about page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from '../../services/ui.service';
-import { Subscription } from 'rxjs'; //
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router'; // We will use this to figure out what route we are on
 
 @Component({
   selector: 'app-header',
@@ -12,7 +13,7 @@ export class HeaderComponent implements OnInit {
   showAddTask: boolean;
   subscription: Subscription;
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService:UiService, private router:Router) {
     //This piece of code is the observer. This constantly listens or subscribes to the Subject from UiService
     this.subscription = this.uiService.onToggle().subscribe(
       (value) => (this.showAddTask = value)
@@ -29,5 +30,9 @@ export class HeaderComponent implements OnInit {
     //This piece of code calls the uiService to update the value in Subject.
     //So in this example, Button is the source as well as the detination(observer) of the Subject
     this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string): boolean {
+    return this.router.url == route;
   }
 }
